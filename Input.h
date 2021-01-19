9d694a634802e87f17b7dcf1f55482a39547f034
Akmulla <d4n73@yandex.ru>
2021-01-19 20:44:10 +0300

    input structure

--- Input.h
+++ Input.h
@@ -1,6 +1,18 @@
 #pragma once
 
+class InputData
+{
+public: 
+	float horizontal;
+	float vertical;
+};
+
 class Input
 {
-
+private:
+	static InputData input;
+public:
+	static const InputData& GetInput();
+	static void SetVerticalAxis(float);
+	static void SetHorizontalAxis(float);
 };
