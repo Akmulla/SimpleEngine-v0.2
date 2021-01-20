d00bdd1f8ec2c7d639618491142d4711f5207848
Akmulla <d4n73@yandex.ru>
2021-01-20 18:07:41 +0300

    GameObject

---
+++ GameObject.cpp
@@ -0,0 +1,31 @@
+#include "GameObject.h"
+
+void GameObject::SetPosition(const DirectX::XMFLOAT3& pos)
+{
+
+}
+
+void GameObject::SetRotation(const DirectX::XMFLOAT3& rot)
+{
+
+}
+
+void GameObject::SetScale(const DirectX::XMFLOAT3& scale)
+{
+
+}
+
+const DirectX::XMFLOAT3& GameObject::GetPosition() const
+{
+    return pos;
+}
+
+const DirectX::XMFLOAT3& GameObject::GetRotation() const
+{
+    return rot;
+}
+
+const DirectX::XMFLOAT3& GameObject::GetScale() const
+{
+    return scale;
+}
--- GameObject.h
+++ GameObject.h
@@ -1,6 +1,18 @@
 #pragma once
+#include <DirectXMath.h>
 
 class GameObject
 {
+public:
+	void SetPosition(const DirectX::XMFLOAT3& pos);
+	void SetRotation(const DirectX::XMFLOAT3& rot);
+	void SetScale(const DirectX::XMFLOAT3& scale);
 
+	const DirectX::XMFLOAT3& GetPosition() const;
+	const DirectX::XMFLOAT3& GetRotation() const;
+	const DirectX::XMFLOAT3& GetScale() const;
+private:
+	DirectX::XMFLOAT3 pos;
+	DirectX::XMFLOAT3 rot;
+	DirectX::XMFLOAT3 scale;
 };
