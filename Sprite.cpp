5b8942df6855ddb90f1c33c10ce7f8598623589f
Akmulla <d4n73@yandex.ru>
2021-01-22 02:33:36 +0300

    Sprite draw

--- Sprite.cpp
+++ Sprite.cpp
@@ -7,8 +7,23 @@ Sprite::Sprite(Graphics* gfx, const wchar_t* spritePath, float width, float heig
 
 	device = gfx->GetDevice();
 	context = gfx->GetContext();
-
+	
 	spriteBatch = std::make_unique<SpriteBatch>(context);
 	ComPtr<ID3D11Resource> resource;
-	CreateWICTextureFromFile(device, spritePath,resource.GetAddressOf(), texture.ReleaseAndGetAddressOf());
+	CreateWICTextureFromFile(device, spritePath,resource.GetAddressOf(), shaderResource.ReleaseAndGetAddressOf());
+
+	ComPtr<ID3D11Texture2D> texture;
+	resource.As(&texture);
+
+	CD3D11_TEXTURE2D_DESC texDesc;
+	texture->GetDesc(&texDesc);
+}
+
+void Sprite::Draw(DirectX::XMFLOAT2 pos)
+{
+	spriteBatch->Begin();
+
+	spriteBatch->Draw(shaderResource.Get(), pos);
+
+	spriteBatch->End();
 }
--- Sprite.h
+++ Sprite.h
@@ -4,15 +4,17 @@
 #include <WICTextureLoader.h>
 #include <string>
 #include <SpriteBatch.h>
+#pragma comment(lib,"DirectXTK.lib")
 
 class Sprite 
 {
 public:
 	Sprite(Graphics*, const wchar_t* spritePath, float width, float height);
+	void Draw(DirectX::XMFLOAT2);
 private:
 	DirectX::XMMATRIX worldMatrix = DirectX::XMMatrixIdentity();
 	std::unique_ptr<DirectX::SpriteBatch> spriteBatch;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> texture;
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> shaderResource;
 	ID3D11Device* device;
 	ID3D11DeviceContext* context;
 };
